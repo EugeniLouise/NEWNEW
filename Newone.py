5fd58b7b2349555da8f5da2756305950639f7a45
Eugenia Subirons <eugenia.subirons@gmail.com>
2023-05-22 13:07:28 +0200

    Primera

---
+++ Newone.py
@@ -0,0 +1,17 @@
+
+
+from tqdm import tqdm
+
+for x in tqdm(range(10000)):
+    pass
+x= 1
+y= 2
+suma= 1 + 2
+resta= 1 - 2
+print(suma)
+print(resta)
+
+print("HOLA")
+
+producto= 2*3
+print(producto)
