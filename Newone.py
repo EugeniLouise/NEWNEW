a74d10b6442c0e7083ee00f3e80e69d457deb2e2
Eugenia Subirons <eugenia.subirons@gmail.com>
2023-05-22 14:09:41 +0200

    Primera

--- Newone.py
+++ Newone.py
@@ -2,18 +2,21 @@
 
 import pandas as pd
 
-# Ruta y nombre de los archivos Excel
-archivo1 = 'C:/Users/Eugenia Subirons/Downloads/Pruebas/C4_medperup_20220101_20220131_18X000000000GDUG.csv'
-archivo2 = 'C:/Users/Eugenia Subirons/Downloads/Pruebas/C4_medperup_20220201_20220228_18X000000000GDUG.csv'
+# Ruta y nombre de los archivos XLSX
+archivo1 = 'C:/Users/Eugenia Subirons/OneDrive - GRUPOTREBOL/Pruebas_PYCHARM/C4_medperup_11.csv'
+archivo2 = 'C:/Users/Eugenia Subirons/OneDrive - GRUPOTREBOL/Pruebas_PYCHARM/C4_medperup_22.csv'
 
-# Leer los archivos CSV
+
+# Leer los archivos CSV utilizando pandas
 df1 = pd.read_csv(archivo1)
 df2 = pd.read_csv(archivo2)
+
 
 # Concatenar los DataFrames uno debajo del otro
 df_combined = pd.concat([df1, df2], ignore_index=True)
 
 # Guardar el DataFrame combinado en un nuevo archivo CSV
-df_combined.to_csv('C:/Users/Eugenia Subirons/Downloads/Pruebas/MEDPERUP_COMBINADO.csv', index=False)
+df_combined.to_csv('C:/Users/Eugenia Subirons/OneDrive - GRUPOTREBOL/Pruebas_PYCHARM/MEDPERUP_COMBINADO.csv', index=False)
 
-print("Los archivos se han combinado correctamente.")
+print("Los archivos se han combinado correctamente después de recortar las filas.")
+
