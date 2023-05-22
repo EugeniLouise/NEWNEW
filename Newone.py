4def2b51027e9817f26eed978569d74820b8e193
Eugenia Subirons <eugenia.subirons@gmail.com>
2023-05-22 13:38:29 +0200

    Primera

--- Newone.py
+++ Newone.py
@@ -1,17 +1,19 @@
 
 
-from tqdm import tqdm
+import pandas as pd
 
-for x in tqdm(range(10000)):
-    pass
-x= 1
-y= 2
-suma= 1 + 2
-resta= 1 - 2
-print(suma)
-print(resta)
+# Ruta y nombre de los archivos Excel
+archivo1 = 'C:/Users/Eugenia Subirons/Downloads/Pruebas/C4_medperup_20220101_20220131_18X000000000GDUG.csv'
+archivo2 = 'C:/Users/Eugenia Subirons/Downloads/Pruebas/C4_medperup_20220201_20220228_18X000000000GDUG.csv'
 
-print("HOLA")
+# Leer los archivos CSV
+df1 = pd.read_csv(archivo1)
+df2 = pd.read_csv(archivo2)
 
-producto= 2*3
-print(producto)
+# Concatenar los DataFrames uno debajo del otro
+df_combined = pd.concat([df1, df2], ignore_index=True)
+
+# Guardar el DataFrame combinado en un nuevo archivo CSV
+df_combined.to_csv('C:/Users/Eugenia Subirons/Downloads/Pruebas/MEDPERUP_COMBINADO.csv', index=False)
+
+print("Los archivos se han combinado correctamente.")
